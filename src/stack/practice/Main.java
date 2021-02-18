1b1932afd964adb1c9e405f2e3086ae7bf8511c1
Matthew McGee <66638526+mpmcgee@users.noreply.github.com>
2021-02-17 19:56:31 -0500

    added dynamic stack functionality

---
+++ src/stack/practice/Main.java
@@ -0,0 +1,39 @@
+package stack.practice;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        DStack nums = new DStack();
+
+        nums.push(15);
+        nums.show();
+        nums.push(8);
+        nums.show();
+        nums.push(10);
+        nums.show();
+        nums.push(10);
+        nums.show();
+        nums.push(10);
+        nums.show();
+
+        nums.pop();
+        nums.show();
+        nums.pop();
+        nums.show();
+        nums.pop();
+        nums.show();
+        nums.pop();
+        nums.show();
+        nums.pop();
+        nums.show();
+
+
+
+
+
+    }
+
+
+
+}
